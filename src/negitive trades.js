a150feb6d738c2614ef88939c8b64247e8bbbcaa
EmperorMew <info@ethmint.io>
2022-12-29 14:28:51 -0600

    fancy changes for my bby

---
+++ src/negitive trades.js
@@ -0,0 +1,50 @@
+// Import the Avalanche.js library and the request library
+import Avalanche from 'avalanche';
+import request from 'request';
+
+// List of facts about the wallet's history
+const facts = [
+  "This wallet has made a total of [number] negative trades.",
+  "The total USD value of negative trades for this wallet is [amount]."
+];
+
+async function displayLoadingScreen() {
+  // Connect to the Avalanche network
+  const ava = await Avalanche.Avalanche.new({
+    host: 'localhost',
+    port: 9650,
+    protocol: 'http'
+  });
+
+  // Get the transaction history for the wallet
+  const transactions = await ava.getTransactions(walletAddress, 10, 0);
+
+  // Filter the transactions to only include negative trades
+  const negativeTrades = transactions.filter(t => t.value < 0);
+
+  // Calculate the total USD value of negative trades
+  const totalUsdValue = negativeTrades.reduce((acc, t) => {
+    // Get the exchange rate for AVAX to USD
+    const exchangeRate = await getExchangeRate();
+
+    // Calculate the USD value of the trade
+    const usdValue = Math.abs(t.value) * exchangeRate;
+
+    return acc + usdValue;
+  }, 0);
+
+  // Select a random fact from the list
+  const fact = facts[Math.floor(Math.random() * facts.length)];
+
+  // Modify the fact to include actual data about the wallet
+  const modifiedFact = fact
+    .replace('[number]', negativeTrades.length)
+    .replace('[amount]', totalUsdValue);
+
+  // Display the modified fact on the loading screen
+  console.log(`Loading...
+
+Did you know? ${modifiedFact}`);
+}
+
+// Call the function to display the loading
